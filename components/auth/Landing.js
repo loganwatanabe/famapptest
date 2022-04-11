b05892327afbf19c4376228bc12ce18ab56cd912
Logan Watanabe <loganwatanabe@gmail.com>
2022-04-11 16:06:50 -0400

    idk

---
+++ components/auth/Landing.js
@@ -0,0 +1,30 @@
+import React, {useState, useContext} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Link } from '@react-navigation/native';
+import {AuthContext} from './AuthProvider'
+
+import * as Linking from 'expo-linking';
+
+
+export default function Landing({navigation}) {
+	const { user, setUser } = useContext(AuthContext);
+  return (
+    <View>
+      <Text>Welcome to the App Landing Screen</Text>
+       <Button
+        onPress={() => navigation.navigate('Main', { screen: 'Auth', params: { screen: 'Login' }})}
+        title="Go to Login"
+      />
+      <Link to={'/login'}>Go to Login</Link>
+      <Button
+        onPress={() => navigation.navigate('Modal')}
+        title="Open Modal"
+      />
+
+      <Button
+        onPress={() => console.log(user)}
+        title="console log"
+      />
+    </View>
+  );
+}
